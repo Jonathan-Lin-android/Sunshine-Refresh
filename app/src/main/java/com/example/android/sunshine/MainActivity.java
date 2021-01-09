47ba1ea09e29ae82f40ea0901363b27917288bac
Jonathan Lin <jonathanlin108@gmail.com>
2021-01-08 17:45:55 -0800

    Added menu and error handling should fetching data fail to user and a loading bar

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -19,6 +19,10 @@ import android.net.Network;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -30,50 +34,59 @@ import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView weatherDataTextView;
+    private TextView mWeatherDataTextView;
+
+    private TextView mErrorMessageTextView;
+
+    private ProgressBar mLoadingIndicatorProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /*
-        // dummy weather data
-        String[] rgDummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
-*/
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_forecast);
 
-        weatherDataTextView = (TextView) findViewById(R.id.tv_weather_data);
-//"94043,USA";
-        new FetchWeatherTask().execute(SunshinePreferences.getPreferredWeatherLocation(this));
-
+        mWeatherDataTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorMessageTextView = (TextView) findViewById(R.id.tv_error_messsage);
+        mLoadingIndicatorProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        loadWeatherData();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int refresh = R.id.action_refresh;
+        switch (item.getItemId()) {
+            case refresh: {
+                loadWeatherData();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     /*
-    search based on location. (user entered text) or dummy location. default preferred location
-    build a url based on query
-    query the database and return response (JSONString)
-    display the string.
-     */
-    class FetchWeatherTask extends AsyncTask<String, Void, String []>
-    {
+            Fetch data from the weather server
+             */
+    class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
         @Override
-        protected String [] doInBackground(final String... strSearch) {
+        protected void onPreExecute() {
+            mLoadingIndicatorProgressBar.setVisibility(View.VISIBLE);
+            showWeatherData();
+        }
+
+        @Override
+        protected String[] doInBackground(final String... strSearch) {
+            if (strSearch.length == 0) {
+                showErrorMessage("Search field is empty");
+                return null;
+            }
             //bilding URL
             URL builtURL = NetworkUtils.buildUrl(strSearch[0]);
 
@@ -81,20 +94,50 @@ public class MainActivity extends AppCompatActivity {
             String[] parsedJSONResponse = null;
             try {
                 String jsonStrResponse = NetworkUtils.getResponseFromHttpUrl(builtURL);
-                parsedJSONResponse= OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonStrResponse);
+                parsedJSONResponse = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonStrResponse);
             } catch (IOException | JSONException e) {
+                showErrorMessage();
                 e.printStackTrace();
             }
             return parsedJSONResponse;
         }
 
         @Override
-        protected void onPostExecute(final String [] jsonStrResult) {
-            //display JSON result / parsed josn result
-            for (String s : jsonStrResult)
-                weatherDataTextView.append(s + "\n\n\n");
+        protected void onPostExecute(final String[] jsonStrResult) {
+            mLoadingIndicatorProgressBar.setVisibility(View.INVISIBLE);
 
+            if (jsonStrResult == null || jsonStrResult.length == 0) {
+                return;
+            }
+            //display JSON result / parsed josn result
+            for (String s : jsonStrResult) {
+                mWeatherDataTextView.append(s + "\n\n\n");
+            }
         }
     }
 
+    private void loadWeatherData() {
+        mWeatherDataTextView.setText("");
+        //"94043,USA";
+        new FetchWeatherTask().execute(SunshinePreferences.getPreferredWeatherLocation(this));
+    }
+
+    private void showErrorMessage()
+    {
+        mErrorMessageTextView.setText(getString(R.string.error_message));
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+        mWeatherDataTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorMessage(String message) {
+        mErrorMessageTextView.setText(message);
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+        mWeatherDataTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showWeatherData() {
+        mWeatherDataTextView.setVisibility(View.VISIBLE);
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+    }
 }
